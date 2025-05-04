7fa22514313f815de41cc8f321b285a251396e6e
KuzminStanislav <a663ve@gmail.com>
2025-05-04 12:47:59 +0400

    changed gui

--- lab_2/gui.py
+++ lab_2/gui.py
@@ -27,14 +27,10 @@ class App(QWidget):
         self.load_button = QPushButton("Load tests data")
         self.load_button.clicked.connect(self.load_data)
 
-        self.run_button = QPushButton("Run tests")
-        self.run_button.clicked.connect(self.run_tests)
-
         self.results_text_edit = QTextEdit()
         self.results_text_edit.setReadOnly(True)
 
         self.layout.addWidget(self.load_button)
-        self.layout.addWidget(self.run_button)
         self.layout.addWidget(self.results_text_edit)
 
         self.setLayout(self.layout)
@@ -46,29 +42,13 @@ class App(QWidget):
         Load data to class
         """
         options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getOpenFileName(self, "Load JSON data",
-                                                   "", "JSON Files (*.json);;All Files (*)", 
+        file_path, _ = QFileDialog.getOpenFileName(self, "Load text data",
+                                                   "", "text Files (*.txt);;All Files (*)", 
                                                    options = options)
         if file_path:
             try:
-                self.settings = load_json_data(file_path, _)
-                self.results_text_edit.append(f"Loaded settings from {file_path}.\n")
-            except Exception as e:
-                QMessageBox.critical(self, "Error", str(e))
-
-
-    def run_tests(self):
-        """
-        Run tests in class
-        """
-        options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getOpenFileName(self, "Load sequence data",
-                                                   "", "JSON Files (*.json);;All Files (*)", 
-                                                   options = options)
-        if file_path:
-            try:
-                sequences = read_sequence(file_path)
-                results = run_tests(sequences)
-                self.results_text_edit.append("\n".join(results) + "\n")
+                with open(file_path, "r") as file:
+                    content = file.read()
+                    self.results_text_edit.setPlainText(content)
             except Exception as e:
                 QMessageBox.critical(self, "Error", str(e))
